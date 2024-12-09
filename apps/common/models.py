1193bacfbbe3a846c74c2e279940d1408aea62d7
mdvYo <Neoculture21@yandex.ru>
2024-12-09 21:08:03 +0300

    first comm

---
+++ apps/common/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+import uuid
+
+from django.utils import timezone
+
+from apps.common.managers import GetOrNoneManager, IsDeletedManager
+
+
+# Create your models here.
+
+
+class BaseModel(models.Model):
+    """
+        A base model class that includes common fields and methods for all models.
+
+        Attributes:
+            id (UUIDField): Unique identifier for the model instance.
+            created_at (DateTimeField): Timestamp when the instance was created.
+            updated_at (DateTimeField): Timestamp when the instance was last updated.
+    """
+
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False, unique=True, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = GetOrNoneManager()
+
+    class Meta:
+        abstract = True
+
+
+class IsDeletedModel(BaseModel):
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    objects = IsDeletedManager()
+
+    def delete(self, *args, **kwargs):
+        '''Soft delete by setting is_deleted=True'''
+
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save(update_fields=['is_deleted', 'deleted_at'])
+
+    def hard_delete(self, *args, **kwargs):
+        super().delete(*args, **kwargs)
+
